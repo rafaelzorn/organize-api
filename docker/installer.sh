27768f763832d1cee70c08adcf2ef5369d57a0b0
rafaelzorn <rafael.zorn@gmail.com>
2021-05-19 11:58:56 -0300

    Improvement: Improvement installer

--- docker/installer.sh
+++ docker/installer.sh
@@ -15,44 +15,64 @@ echo ""
 docker-compose up -d
 
 echo ""
-echo "=================================================> 20%"
+echo "=================================================> 14%"
 echo ""
 
 echo ""
-echo "2) Instalando dependências via composer"
+echo "2) Criando arquivo .env"
 echo ""
 
-docker exec organize-api composer install
+docker exec organize-api cp .env.example .env
 
 echo ""
-echo "=================================================> 40%"
+echo "=================================================> 28%"
 echo ""
 
 echo ""
-echo "3) Executando migrations"
+echo "3) Instalando dependências via composer"
+echo ""
+
+docker exec organize-api composer install --ignore-platform-req=php
+
+echo ""
+echo "=================================================> 42%"
+echo ""
+
+echo ""
+echo "4) Executando migrations"
 echo ""
 
 docker exec organize-api php artisan migrate
 
 echo ""
-echo "=================================================> 60%"
+echo "=================================================> 56%"
 echo ""
 
 echo ""
-echo "4) Executando seeders"
+echo "5) Executando seeders"
 echo ""
 
 docker exec organize-api php artisan db:seed
 
 echo ""
-echo "=================================================> 80%"
+echo "=================================================> 70%"
 echo ""
 
 echo ""
-echo "5) Gerando chave secreta JWT"
+echo "6) Gerando chave secreta JWT"
 echo ""
 
 docker exec organize-api php artisan jwt:secret
+
+echo ""
+echo "=================================================> 84%"
+echo ""
+
+echo ""
+echo "7) Rodando testes de integração"
+echo ""
+
+docker exec organize-api vendor/bin/phpunit tests/Integration/
 
 echo ""
 echo "=================================================> 100%"

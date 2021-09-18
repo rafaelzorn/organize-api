6a96807593ed3b949cf2f242a90c0154a314b655
rafaelzorn <rafael.zorn@gmail.com>
2021-09-17 22:52:58 -0300

    Refactor: MovementCategorySeeder

--- database/seeders/MovementCategorySeeder.php
+++ database/seeders/MovementCategorySeeder.php
@@ -29,76 +29,27 @@ class MovementCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        $movementCategory = $this->movementCategoryRepository->find(1);
+        $movementCategories = [
+            1  => 'Casa',
+            2  => 'Educação',
+            3  => 'Eletrônicos',
+            4  => 'Lazer',
+            5  => 'Outros',
+            6  => 'Restaurante',
+            7  => 'Saúde',
+            8  => 'Serviços',
+            9  => 'Supermercado',
+            10 => 'Transporte',
+            11 => 'Vestuário',
+            12 => 'Viagem',
+        ];
 
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 1, 'name' => 'Casa']);
-        }
+        foreach ($movementCategories as $id => $movementCategory) {
+            if ($this->movementCategoryRepository->find($id)) {
+                continue;
+            }
 
-        $movementCategory = $this->movementCategoryRepository->find(2);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 2, 'name' => 'Educação']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(3);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 3, 'name' => 'Eletrônicos']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(4);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 4, 'name' => 'Lazer']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(5);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 5, 'name' => 'Outros']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(6);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 6, 'name' => 'Restaurante']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(7);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 7, 'name' => 'Saúde']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(8);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 8, 'name' => 'Serviços']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(9);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 9, 'name' => 'Supermercado']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(10);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 10, 'name' => 'Transporte']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(11);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 11, 'name' => 'Vestuário']);
-        }
-
-        $movementCategory = $this->movementCategoryRepository->find(12);
-
-        if (empty($movementCategory)) {
-            $this->movementCategoryRepository->create(['id' => 12, 'name' => 'Viagem']);
+            $this->movementCategoryRepository->create(['id' => $id, 'name' => $movementCategory]);
         }
     }
 }
